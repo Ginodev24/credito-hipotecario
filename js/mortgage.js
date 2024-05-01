be739bc47ce52f1dddb3e64500cb66dae4fd4a6f
ginodev <ginodev24@gmail.com>
2024-05-01 11:39:03 -0700

    evalua 90/100 del porcentaje

--- js/mortgage.js
+++ js/mortgage.js
@@ -9,14 +9,16 @@ const calculateMortgage = (e) =>{
     const MONTHS_ON_YEAR = 12;
 
     const mortgage = {
+        costo_total_inmueble : 0,
         total_prestamo  : 0,
         total_intereses : 0,
         cuota_mensual   : 0
     };
 
-    mortgage.total_prestamo  = costo_total - cuota;
-    mortgage.total_intereses = mortgage.total_prestamo * interes / 100;
-    mortgage.cuota_mensual   = ( mortgage.total_prestamo + mortgage.total_intereses ) / ( plazo_anio * MONTHS_ON_YEAR );
+    mortgage.costo_total_inmueble = costo_total;
+    mortgage.total_prestamo       = costo_total - cuota;
+    mortgage.total_intereses      = mortgage.total_prestamo * interes / 100;
+    mortgage.cuota_mensual        = ( mortgage.total_prestamo + mortgage.total_intereses ) / ( plazo_anio * MONTHS_ON_YEAR );
 
     //alert(mortgage.cuota_mensual);
     output_mortgage(mortgage);
@@ -25,6 +27,16 @@ const calculateMortgage = (e) =>{
 const output_mortgage = (final_mortgage) => {
     document.getElementById("o_monto_prestamo").innerHTML = value_to_dollar( final_mortgage.total_prestamo );
     document.getElementById("o_cuota_mensual").innerHTML  = value_to_dollar( final_mortgage.cuota_mensual );
+
+    let totalPrestamoPorcentaje = 0;
+    totalPrestamoPorcentaje = final_mortgage.total_prestamo * 100 / final_mortgage.costo_total_inmueble;
+    alert(totalPrestamoPorcentaje);
+    if(totalPrestamoPorcentaje > 90){
+        //document.getElementById("o_monto_prestamo").className = "alertaPorcentaje";
+        document.getElementById("o_monto_prestamo").classList.add("alertaPorcentaje");
+    }else{
+        document.getElementById("o_monto_prestamo").classList.remove("alertaPorcentaje");
+    }
 }
 
 const reset_form = () => {
